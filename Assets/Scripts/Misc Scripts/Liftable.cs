446e858e66e100f29675be5e0043cf5fa58e595f
Paul Sinnett <paul.sinnett@gmail.com>
2017-11-29 19:28:01 +0000

    pickup and attach

--- Assets/Scripts/Misc Scripts/Liftable.cs
+++ Assets/Scripts/Misc Scripts/Liftable.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class Liftable : MonoBehaviour {
 
 	public Rigidbody rb;
-	public float thrust;
 
 	// Use this for initialization
 	void Start () {
-
+        rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rb = GetComponent<Rigidbody> ();
 	}
 
-	public void PickingUp(Transform guide){
+	public void Attach(Transform guide){
 		GetComponent<Rigidbody> ().useGravity = false;
 		GetComponent<Rigidbody> ().isKinematic = true;
 		transform.position = guide.position;
@@ -31,15 +29,10 @@ public class Liftable : MonoBehaviour {
 		transform.parent = null;
 	}
 
-	public void throwing(){
-		if (transform.parent != null) {
-			GetComponent<Rigidbody> ().useGravity = true;
-			GetComponent<Rigidbody> ().isKinematic = false;
-			transform.parent = null;
-			rb.AddForce (transform.forward * thrust, ForceMode.Impulse);
-			print ("throw");
-		}
-	}
+    public bool IsAttached()
+    {
+        return transform.parent != null;
+    }
 
 	public void shadowZone(Transform shadowPosition){
 		transform.parent = null;
