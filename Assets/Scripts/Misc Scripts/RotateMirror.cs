1271f1c6c5325cefc9b62abc465cc75bad7716d2
notleeandrew <leeandrew1296@hotmail.com>
2017-12-04 03:10:48 +0000

    Prototype building

--- Assets/Scripts/Misc Scripts/RotateMirror.cs
+++ Assets/Scripts/Misc Scripts/RotateMirror.cs
@@ -8,10 +8,17 @@ public class RotateMirror : MonoBehaviour {
 
 	void OnTriggerStay (Collider col){
 		if (col.gameObject.tag == "Player") {
-			if (Input.GetKeyDown(KeyCode.E)) {
-				transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y + yRotation, 0);
-				print ("I'm turning");
-			}
-		}
+            if (Input.GetKey(KeyCode.E)) {
+                transform.Rotate(Vector3.up * yRotation * Time.deltaTime);
+                //transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y + yRotation, 0);
+                print("I'm turning");
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate(Vector3.down * yRotation * Time.deltaTime);
+                //transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y + yRotation, 0);
+                print("I'm turning");
+            }
+        }
 	}
 }
